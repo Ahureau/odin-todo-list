ca3447eac659ea3634505fde4c4decd2b3bb3247
Alex H <53427235+Ahureau@users.noreply.github.com>
2024-05-16 09:52:49 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Odin to-do list',
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
